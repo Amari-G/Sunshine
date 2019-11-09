e871eb247ce93f853cf926a376401c0d2c11a7d0
Amari Garrett <amariwgarrett@gmail.com>
2019-11-09 10:08:37 -0500

    Added Profile fragment and associated additions

--- app/src/main/java/com/example/sunshine/Main2Activity.java
+++ app/src/main/java/com/example/sunshine/Main2Activity.java
@@ -44,7 +44,7 @@ public class Main2Activity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_profile, R.id.nav_moments,
+                R.id.nav_profile, R.id.nav_moments,
                 R.id.nav_tools, R.id.nav_share, R.id.nav_send)
                 .setDrawerLayout(drawer)
                 .build();
